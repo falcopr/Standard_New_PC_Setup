224fac2d5eca94840ce8ac7e795337333cab96d6
Falco Prescher <falcoprescher@googlemail.com>
2016-11-11 02:09:08 +0100

    Additional tools to install through chocolatey

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -1,9 +1,9 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $programs = @("install", "7zip", "adobereader", "adwcleaner", "atom", "autohotkey", "blender",`
-"cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "git", "googlechrome", "greenshot",`
+"cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "eclipse", "git", "googlechrome", "greenshot", "googledrive",`
 "inkscape", "jdk8", "jdownloader", "keepass", "nano", "nodejs", "npm", "notepadplusplus",`
-"openvpn", "paint.net", "packer", "pdfcreator", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
+"openvpn", "paint.net", "packer", "pdfcreator", "powershell", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
 "steam", "skype", "synergy", "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "virtualbox", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go", "-y")
 & choco $programs
 
