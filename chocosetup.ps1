72b09ed035b88592070aa7122482d9df53509001
Falco Prescher <falcoprescher@googlemail.com>
2016-11-11 01:21:21 +0100

    Bugfixing and additional apps vagrant and packer

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -1,9 +1,9 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$programs = @("7zip", "adobereader", "adwcleaner", "atom", "autohotkey", "blender",`
+$programs = @("install", "7zip", "adobereader", "adwcleaner", "atom", "autohotkey", "blender",`
 "cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "git", "googlechrome", "greenshot",`
 "inkscape", "jdk8", "jdownloader", "keepass", "nano", "nodejs", "npm", "notepadplusplus",`
-"openvpn", "paint.net", "pdfcreator", "putty", "python", "rufus", "sourcetree", "speedcrunch",`
+"openvpn", "paint.net", "packer", "pdfcreator", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
 "steam", "skype", "synergy", "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "virtualbox", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go", "-y")
 
 & choco $programs
