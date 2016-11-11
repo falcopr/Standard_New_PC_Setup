166ad1ede854f0cca8f942bc0cdde348c4847c32
Falco Prescher <falcoprescher@googlemail.com>
2016-11-11 01:38:55 +0100

    Install atom packages

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -5,5 +5,9 @@ $programs = @("install", "7zip", "adobereader", "adwcleaner", "atom", "autohotke
 "inkscape", "jdk8", "jdownloader", "keepass", "nano", "nodejs", "npm", "notepadplusplus",`
 "openvpn", "paint.net", "packer", "pdfcreator", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
 "steam", "skype", "synergy", "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "virtualbox", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go", "-y")
-
 & choco $programs
+
+$apmpackages = @("install", "angularjs", "atom-beautify", "atom-ternjs", "atom-yeoman", "autocomplete-ionic-framework", "browser-plus", "command-toolbar", "docker", "editorconfig", "file-icons", "format-sql", "git-plus", "ionic-framework-snippets", "ionic-preview", "json-schema", "keybinding-cheatsheet", "keyboard-localization",`
+"language-cshtml", "language-docker", "language-fsharp", "language-powershell", "language-t4", "linter", "linter-chktex", "linter-csslint", "linter-docker", "linter-gcc", "linter-htmlhint", "linter-jshint", "linter-less", "linter-sass-lint", "linter-tidy", "minimap", "monokai", "omnisharp-atom", "open-conemu-here",`
+"script", "symbols-tree-view", "tree-view-filter", "tree-view-finder", "zentabs")
+& apm $apmpackages
