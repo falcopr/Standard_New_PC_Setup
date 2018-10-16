bd882334aa8c07b709a42f2d7269d6bf16e3c45f
Falco Prescher <falcoprescher@googlemail.com>
2018-10-16 13:22:21 +0200

    More tools for docker

--- chocosetup_workstation.ps1
+++ chocosetup_workstation.ps1
@@ -1,7 +1,7 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $programs = @("install", "7zip", "adobereader", "autohotkey",`
-"cdrtfe", "conemu", "dotpeek", "fiddler", "firefox", "eclipse", "git", "googlechrome", "greenshot",`
+"conemu", "docker", "docker-machine", "docker-compose", "dotpeek", "fiddler", "firefox", "eclipse", "git", "googlechrome", "greenshot",`
 "inkscape", "keepass", "nano", "nodejs", "notepadplusplus",`
 "openvpn", "paint.net", "packer", "pdfcreator", "powershell", "putty", "python", "microsoft-teams", "sourcetree", "speedcrunch", "vagrant",`
 "sysinternals", "vim", "vlc", "visualstudiocode", "wget", "winscp", "wireshark",`
