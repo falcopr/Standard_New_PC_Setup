94353fd9420f1c43966fbd42aab210aee346714d
Falco Prescher <falcoprescher@googlemail.com>
2019-04-06 16:35:17 +0200

    No JDownloader

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -2,7 +2,7 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 
 $programs = @("install", "7zip", "adobereader", "adwcleaner", "atom", "autohotkey", "blender",`
 "cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "eclipse", "git", "googlechrome", "greenshot", "googledrive",`
-"inkscape", "jdk8", "jdownloader", "keepass", "nano", "nodejs", "npm", "notepadplusplus",`
+"inkscape", "jdk8", "keepass", "nano", "nodejs", "npm", "notepadplusplus",`
 "openvpn", "paint.net", "packer", "pdfcreator", "powershell", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
 "steam", "synergy", "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "vmwareworkstation", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go",`
 "gpu-z", "cpu-z", "openhardwaremonitor", "libreoffice", "yed", "-y", "--allow-empty-checksums")
