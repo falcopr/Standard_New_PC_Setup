a47e24670e8b65475f3aa2a52c34ba11f2d8890e
Falco Prescher <falcoprescher@googlemail.com>
2019-08-25 14:22:32 +0200

    Removing Fiddler

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -1,7 +1,7 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $programs = @("install", "7zip", "adwcleaner", "atom", "autohotkey", "blender",`
-"cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "eclipse", "git", "chromium", "greenshot",`
+"cdrtfe", "conemu", "dotpeek", "firefox", "eclipse", "git", "chromium", "greenshot",`
 "inkscape", "keepass", "nano", "nodejs", "notepadplusplus",`
 "openvpn", "paint.net", "packer", "pdfcreator", "powershell", "pdfsam.install", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
 "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "vmwareworkstation", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go",`
