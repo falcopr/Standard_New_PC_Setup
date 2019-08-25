a97a67b54721fa766efb9c575b1fef6059f68a3f
Falco Prescher <falcoprescher@googlemail.com>
2019-08-25 14:18:04 +0200

    Removing/Updating programs

--- chocosetup.ps1
+++ chocosetup.ps1
@@ -1,9 +1,9 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$programs = @("install", "7zip", "adobereader", "adwcleaner", "atom", "autohotkey", "blender",`
-"cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "eclipse", "git", "googlechrome", "greenshot", "googledrive",`
-"inkscape", "jdk8", "keepass", "nano", "nodejs", "notepadplusplus",`
-"openvpn", "paint.net", "packer", "pdfcreator", "powershell", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
-"steam", "synergy", "sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "vmwareworkstation", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go",`
-"gpu-z", "cpu-z", "openhardwaremonitor", "libreoffice", "yed", "-y", "--allow-empty-checksums")
+$programs = @("install", "7zip", "adwcleaner", "atom", "autohotkey", "blender",`
+"cdrtfe", "conemu", "dotpeek", "fiddler4", "firefox", "eclipse", "git", "chromium", "greenshot",`
+"inkscape", "keepass", "nano", "nodejs", "notepadplusplus",`
+"openvpn", "paint.net", "packer", "pdfcreator", "powershell", "pdfsam.install", "putty", "python", "rufus", "sourcetree", "speedcrunch", "vagrant",`
+"sysinternals", "teamspeak", "teamviewer", "texmaker", "thunderbird", "vim", "vmwareworkstation", "vlc", "visualstudiocode", "wget", "winscp", "wireshark", "x2go",`
+"gpu-z", "cpu-z", "hwinfo", "libreoffice", "yed", "-y", "--allow-empty-checksums")
 & choco $programs
