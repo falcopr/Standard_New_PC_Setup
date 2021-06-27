0a7061148c5fb143a4e03e12c463edcf1d61ed0f
Falco P <1677412+falcopr@users.noreply.github.com>
2021-06-27 13:45:18 +0200

    Update chocosetup_workstation.ps1

--- chocosetup_workstation.ps1
+++ chocosetup_workstation.ps1
@@ -1,9 +1,9 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 $programs = @("install", "7zip", "adobereader", "autohotkey",`
-"conemu", "docker", "docker-machine", "docker-compose", "dotpeek", "fiddler", "firefox", "eclipse", "git", "googlechrome", "greenshot",`
+"conemu", "docker", "docker-machine", "docker-compose", "dotpeek", "firefox", "eclipse", "git", "googlechrome", "greenshot",`
 "inkscape", "keepass", "nano", "nodejs", "notepadplusplus",`
 "openvpn", "paint.net", "packer", "pdfcreator", "powershell", "putty", "python", "sourcetree", "speedcrunch", "vagrant",`
 "sysinternals", "vim", "vlc", "visualstudiocode", "wget", "winscp", "wireshark",`
-"libreoffice", "yed", "dotnetcore", "-y", "--allow-empty-checksums")
+"libreoffice", "yed", "dotnetcore", "-y")
 & choco $programs
