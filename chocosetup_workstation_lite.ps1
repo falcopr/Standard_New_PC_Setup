ee822a0aec8d08f67cf2a7315d044a549c58057a
Falco P <1677412+falcopr@users.noreply.github.com>
2022-02-15 20:31:54 +0100

    Update chocosetup_workstation_lite.ps1

--- chocosetup_workstation_lite.ps1
+++ chocosetup_workstation_lite.ps1
@@ -5,7 +5,7 @@ $programs = @("install",`
 # Minimum Development Tools
 "7zip", "docker", "docker-machine", "docker-compose", "firefox", "vscode", `
 "git", "chromium", "greenshot", "keepass", "notepadplusplus", "nvm.portable", "openvpn", "yarn", `
-"powershell", "putty", "winscp", "dotnet-windowshosting", "sql-server-management-studio", "iis-arr", "urlrewrite" `
+"powershell", "putty", "winscp", "dotnet-windowshosting", "dotnet-sdk", "sql-server-management-studio", "iis-arr", "urlrewrite" `
 # Additional Nice Too Have Tools
 # "adobereader", "autohotkey", "conemu", "inkscape", "nano", "paint.net", "packer", "pdfcreator",  "python", "sourcetree", "speedcrunch", "vagrant",`
 # "sysinternals", "vim", "vlc", "wget", "wireshark", "libreoffice", "yed", `
